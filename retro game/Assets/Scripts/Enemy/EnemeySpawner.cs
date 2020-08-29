99204c137e08dc9afd981449bf11d745d65ca10d
Radgar123 <38327161+Radgar123@users.noreply.github.com>
2020-08-29 10:39:33 +0200

    Create  a new Enemies

--- retro game/Assets/Scripts/Enemy/EnemeySpawner.cs
+++ retro game/Assets/Scripts/Enemy/EnemeySpawner.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class EnemeySpawner : MonoBehaviour
 {
-      [SerializeField] private GameObject[] enemy;               
-      [SerializeField] private  float spawnTime = 3f;   
+    [SerializeField] private GameObject[] enemy;
+    [SerializeField] private Transform[] spawnPoints;
 
-      [SerializeField] private Transform[] spawnPoints;         
+    [SerializeField] private  float spawnTime = 3f;
+
+    [SerializeField] private float min;
+    [SerializeField] private float max;
+    
+
+              
 
 
-      void Start ()
-      {
-          InvokeRepeating ("Spawn", spawnTime, spawnTime);
-      }
+     void Start ()
+     {
+         InvokeRepeating ("Spawn", spawnTime, spawnTime);
+     }
 
-      void Spawn ()
+    private void Update()
+    {
+        RandomSpawnTime();
+    }
+
+    void Spawn ()
       {
           int enemyIndex = Random.Range(0, enemy.Length);
           int spawnPointIndex = Random.Range (0, spawnPoints.Length);
           Instantiate (enemy[enemyIndex] , spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
       }
+
+    void RandomSpawnTime() 
+    {
+        spawnTime = Random.Range(min, max);
+    }
 }
