8eb349577797077542a1f224ef1ebbb58d73c59f
Radgar123 <38327161+Radgar123@users.noreply.github.com>
2020-09-17 17:41:53 +0200

    Create Boss Script

---
+++ retro game/Assets/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider bossLife;
+
+    public void SetMaxHealth(int health) 
+    {
+        bossLife.maxValue = health;
+        bossLife.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        bossLife.value = health;
+    }
+}
